c16629d3870baac3afa09145e2b5c1a85b317359
Владислав Александрович Бегачев <thevlhere@gmail.com>
2021-11-26 23:06:06 +0300

    Done

--- homework00/hello.py
+++ homework00/hello.py
@@ -1,7 +1,7 @@
 def get_greeting(name: str) -> str:
-    pass
+    return name
 
 
 if __name__ == "__main__":
     message = get_greeting("World")
-    print(message)
+    print('Hello, ' + message + '!')
