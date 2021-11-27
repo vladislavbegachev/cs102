104b22fe5f7cdd9bcde8a7d4578e95bcf50b1803
Владислав Александрович Бегачев <thevlhere@gmail.com>
2021-11-27 22:37:49 +0300

    Добавлено решение для функции приветствия

--- homework00/hello.py
+++ homework00/hello.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def get_greeting(name: str) -> str:
     return 'Hello, ' + name + '!'
 
@@ -6,12 +5,3 @@ def get_greeting(name: str) -> str:
 if __name__ == "__main__":
     message = get_greeting("World")
     print(message)
-=======
-def get_greeting(name: str) -> str:
-    message = "Hello, " + name + "!"
-    return message
-
-if __name__ == "__main__":
-    message = get_greeting("World")
-    print(message)
->>>>>>> 9ba6e5012e73fb8fe67275fc4d4d73f17a13d566
