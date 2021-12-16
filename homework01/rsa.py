1abb3b8f3e85abaa743cddaf6748906f19996ebb
vladislavbegachev <thevlhere@gmail.com>
2021-12-16 21:29:15 +0300

    black

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -62,9 +62,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     return y % phi
 
 
-def generate_keypair(
-    p: int, q: int
-) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -17,7 +17,6 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     code_nums = []
     capital_indicator = []
 
-
     for letter in plaintext:
         plain.append(letter)
 
@@ -41,13 +40,13 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
             index += 32
             code_nums.append(index)
 
-    i  =  0
+    i = 0
     while i < len(plain_nums):
         for index in plain_nums:
             if plain_nums[i] + code_nums[i] <= 25:
                 plain_nums[i] += code_nums[i]
             else:
-               plain_nums[i] += code_nums[i] - 1 - 25
+                plain_nums[i] += code_nums[i] - 1 - 25
             code_nums.append(code_nums[i])
             plain_nums[i] += capital_indicator[i]
             i += 1
@@ -56,7 +55,7 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for index in plain_nums:
         ciphertext_list.append(chr(plain_nums[i] + 97))
         i += 1
-    
+
     ciphertext = "".join(map(str, ciphertext_list))
 
     return ciphertext
@@ -79,7 +78,6 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     code = []
     code_nums = []
     capital_indicator = []
-
 
     for letter in ciphertext:
         plain.append(letter)
@@ -112,7 +110,7 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
             if plain_nums[i] - code_nums[i] >= 0:
                 plain_nums[i] -= code_nums[i]
             else:
-               plain_nums[i] -= code_nums[i] - 1 - 25
+                plain_nums[i] -= code_nums[i] - 1 - 25
             code_nums.append(code_nums[i])
             plain_nums[i] += capital_indicator[i]
             i += 1
@@ -121,7 +119,7 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     for index in plain_nums:
         ciphertext_list.append(chr(plain_nums[i] + 97))
         i += 1
-    
+
     plaintext = "".join(map(str, ciphertext_list))
-    
+
     return plaintext
