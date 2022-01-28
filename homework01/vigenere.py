b09fa6d6f1ab84de097f4e1f9d44b4ae0b16a046
Владислав Александрович Бегачев <90193780+vladislavbegachev@users.noreply.github.com>
2022-01-28 18:31:48 +0300

    Add files via upload

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,125 +1,29 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    ciphertext_list = []
-    plain = []
-    plain_nums = []
-    code = []
-    code_nums = []
-    capital_indicator = []
-
-    for letter in plaintext:
-        plain.append(letter)
-
-    for coding_letter in keyword:
-        code.append(coding_letter)
-
-    for letter in plain:
-        index = ord(letter) - 97
-        if index > -1:
-            plain_nums.append(index)
-            capital_indicator.append(0)
-        else:
-            index += 32
-            plain_nums.append(index)
-            capital_indicator.append(-32)
-    for letter in code:
-        index = ord(letter) - 97
-        if index > -1:
-            code_nums.append(index)
-        else:
-            index += 32
-            code_nums.append(index)
-
-    i = 0
-    while i < len(plain_nums):
-        for index in plain_nums:
-            if plain_nums[i] + code_nums[i] <= 25:
-                plain_nums[i] += code_nums[i]
+    keyword = keyword.upper()
+    for i, letter in enumerate(plaintext):
+        if letter.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord("A")
+            if letter.islower():
+                ciphertext += chr((ord(letter) + shift - ord("a")) % 26 + ord("a"))
             else:
-                plain_nums[i] += code_nums[i] - 1 - 25
-            code_nums.append(code_nums[i])
-            plain_nums[i] += capital_indicator[i]
-            i += 1
-
-    i = 0
-    for index in plain_nums:
-        ciphertext_list.append(chr(plain_nums[i] + 97))
-        i += 1
-
-    ciphertext = "".join(map(str, ciphertext_list))
-
+                ciphertext += chr((ord(letter) + shift - ord("A")) % 26 + ord("A"))
+        else:
+            ciphertext += letter
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
-    ciphertext_list = []
-    plain = []
-    plain_nums = []
-    code = []
-    code_nums = []
-    capital_indicator = []
-
-    for letter in ciphertext:
-        plain.append(letter)
-
-    for coding_letter in keyword:
-        code.append(coding_letter)
-
-    for letter in plain:
-        index = ord(letter) - 97
-        if index > -1:
-            plain_nums.append(index)
-            capital_indicator.append(0)
-        else:
-            index += 32
-            plain_nums.append(index)
-            capital_indicator.append(-32)
-
-    for letter in code:
-        index = ord(letter) - 97
-        if index > -1:
-            code_nums.append(index)
-        else:
-            index += 32
-            code_nums.append(index)
-
-    i = 0
-
-    while i < len(plain_nums):
-        for index in plain_nums:
-            if plain_nums[i] - code_nums[i] >= 0:
-                plain_nums[i] -= code_nums[i]
+    plaintext = ""
+    keyword = keyword.upper()
+    for i, letter in enumerate(ciphertext):
+        if letter.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord("A")
+            if letter.islower():
+                plaintext += chr((ord(letter) - shift - ord("a")) % 26 + ord("a"))
             else:
-                plain_nums[i] -= code_nums[i] - 1 - 25
-            code_nums.append(code_nums[i])
-            plain_nums[i] += capital_indicator[i]
-            i += 1
-
-    i = 0
-    for index in plain_nums:
-        ciphertext_list.append(chr(plain_nums[i] + 97))
-        i += 1
-
-    plaintext = "".join(map(str, ciphertext_list))
+                plaintext += chr((ord(letter) - shift - ord("A")) % 26 + ord("A"))
+        else:
+            plaintext += letter
 
     return plaintext
